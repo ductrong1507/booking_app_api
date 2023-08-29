b6a0f31e7f145de980817719bc052e715b23c1a7
ductrong1507 <111946172+ductrong1507@users.noreply.github.com>
2023-08-29 23:03:51 +0700

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
   return mongoose.connect(
-    "mongodb+srv://tdtrong07:TLYpzByp02mkAl4r@cluster0.zdjmdtn.mongodb.net/asm_02?retryWrites=true&w=majority"
+    "mongodb+srv://<username>:<password>@cluster0.zdjmdtn.mongodb.net/asm_02?retryWrites=true&w=majority"
   );
 };
 
